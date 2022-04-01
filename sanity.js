679ced9b3f6fe6b09a3004e88c96cd129631c3f2
brandon9598 <brandon9598@gmail.com>
2022-04-01 14:48:12 -0400

    hooked up fe to sanity

--- sanity.js
+++ sanity.js
@@ -2,10 +2,11 @@ import { createClient } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production',
+  projectId: '3apjgndb',
   apiVersion: '2021-04-01',
-  useCdn: process.env.NODE_ENV === 'production',
+  useCdn: false,
+  token: process.env.SANITY_API_TOKEN,
 }
 
 export const sanityClient = createClient(config)
